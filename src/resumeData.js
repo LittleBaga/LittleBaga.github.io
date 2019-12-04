d048f1d3b5a8c723523a7ad6e83b89fce9b1e1d2
LittleBaga <ilmce@ehotmail.com>
2019-12-04 19:00:39 +0100

    resume commit

---
+++ src/resumeData.js
@@ -0,0 +1,122 @@
+
+let resumeData = {
+    "name": "Ilma Curic",
+    "role": "Frontend Developer",
+    "emailID":"ilmakovacevic71@gmail.com",
+    "Phonenumber":"+467 218 872 08",
+    "adressID":"Billebergavägen 141",
+    "CityID":"Annelöv",
+    "roleDescription": "a mom to a little boy and a millenial.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/ilma-curic-56212517a//",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/LittleBaga",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am a student at Malmรถ Yrkeshรถgskola and Studying Frontend development and marketing. On my free time I like to spend time with my family and learn about new things. I like to spend time on my computer, right now my new project is learing more about linux and try out what I can do more with it. ",
+    "address":"Sweden",
+    "education":[
+      {
+        "SchoolName":"Malmรถ Yrkeshรถgskola",
+        "specialization":"Frontend Developer",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2019"
+      },
+      {
+        "SchoolName":"Cybergymnasiet",
+        "specialization":"Ekonomi",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2014"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Landskrona Stad",
+        "specialization":"Barnskรถtare",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2017"
+      }
+    ],
+    "skillsDescription":"My skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      { 
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"WordPress"
+      },
+      {
+        "skillname":"Office package"
+      },
+      {
+        "skillname":"Adobe package"
+      },
+      {
+        "skillname":"React Studio"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Simple HTML and CSS page",
+        "description":"First project in school building a simple page in HTML and CSS, there is a bit JavaScript in there as well. This is my first page ever that I made and coded. Click on the picture to follow the link to the Github repo!",
+        "imgurl":"images/sample-image.jpg",
+        "urlimg":"https://github.com/LittleBaga/ilmaa-",
+
+      },
+      {
+        "name":"Wordpress shop/blog",
+        "description":"A webbshop and blog made in Wordpress. I used a theme that works well with accessbility, responsive and more flexible. I installed plugins like Stripe for payment, mailchimp for subscrition, jetpack and so other plugins. I made this in the second course in school as we needed to learn about Wordpress and marketing. There is a trouble with the database and the site is currenty now working. Click on the picture to follow the link!",
+        "imgurl":"images/littlebaga.jpeg",
+        "urlimg": "https://ilmce.my.nu",
+      },
+      {
+        "name":"uCalc Calculator",
+        "description":"Calculator I made in uCalc.pro on my internship at SenseNode. It is for new custommers to calculate how much power consumption thay can save also how mych money they can save. I installed the uCalc plunig in wordpress and connected the calculator to wordpress plugin. Click on the picture to follow the link!",  
+        "imgurl":"images/project3.png",
+        "urlimg":"http://sensenode.com",
+      },
+      {
+        "name":"Currecy Coverter",
+        "description":"Me and my group made a currency calculator in React, it calculates dirrerent currencies that is avalible in the app. You need to login to use it and to be able to save your favorite currencies, all of the data goes to firebase where it gets stored and pulls data from. This is my first project I made in React. Click on the picture to follow the link to the Github repo!",
+        "imgurl":"images/converter.jpg",
+        "urlimg":"https://github.com/LittleBaga/bananaconverters"
+      },
+      {
+        "name":"Portfolio",
+        "description":"My first offical Portfolio! I made it with ReactJS, I plan to make a new one with React and Gatsby just to test the waters with Gatsby since many recommended it. Anyway, Click on the picture to follow the link to the Github repo!",
+        "imgurl":"images/ex.jpg",
+        "urlimg":"https://github.com/LittleBaga/LittleBaga.github.io"
+      }
+    ],
+        "Language":[
+        {
+            "languagename":"Bosnian",
+            "description":"Native",
+        },
+        {
+            "languagename":"Swedish",
+            "description":"Fluent",
+        },
+        {
+            "languagename":"English",
+            "description":"Fluent",
+        }
+    ],
+  }
+  
+  export default resumeData
